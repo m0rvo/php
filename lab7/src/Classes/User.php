d2040dc4fef23355e5a06988d05f098fd6ff6cae
m0rvo <vovam387@gmail.com>
2025-12-23 22:21:10 +0300

    Update User.php

--- lab7/src/Classes/User.php
+++ lab7/src/Classes/User.php
@@ -1,25 +1,41 @@
 <?php
 namespace src\Classes;
 
+/**
+ * Класс User описывает базового пользователя системы.
+ * Демонстрирует использование деструктора и продвижения свойств.
+ */
 class User
 {
-
-    public $name;
-    public $login;
-    private $password;
-    public function __construct($name, $login, $password)
-    {
-        $this->name = $name;
-        $this->login = $login;
-        $this->password = $password;
+    /**
+     * Конструктор класса с использованием Property Promotion (PHP 8.0+)
+     * * @param string $name Имя пользователя
+     * @param string $login Логин пользователя
+     * @param string $password Пароль (приватное свойство)
+     */
+    public function __construct(
+        public string $name, 
+        public string $login, 
+        private string $password
+    ) {
+        // Свойства создаются и присваиваются автоматически
     }
-    public function showInfo(): string   {
+
+    /**
+     * Возвращает HTML-разметку с информацией о пользователе
+     */
+    public function showInfo(): string 
+    {
         return "<div class=\"user-info\">
                     <h3>User Info</h3>
                     <p><strong>Name:</strong> {$this->name}</p>
                     <p><strong>Login:</strong> {$this->login}</p>
                 </div>";
     }   
+
+    /**
+     * Деструктор срабатывает при удалении объекта из памяти
+     */
     public function __destruct()
     {
         echo "<p>Пользователь {$this->login} удален.</p>";
