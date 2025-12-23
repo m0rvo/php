393f00f96234d913926f2ef77bd7eb1580657b98
m0rvo <vovam387@gmail.com>
2025-12-23 22:20:44 +0300

    Update users.php

--- lab7/users.php
+++ lab7/users.php
@@ -1,27 +1,28 @@
 <?php
 declare(strict_types=1);
 
+// Регистрация автозагрузчика классов
+spl_autoload_register(function ($class): void {
+    // Превращаем namespace в путь: src\Classes\User -> src/Classes/User.php
+    $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+
 use src\Classes\User;
 use src\Classes\SuperUser;
 
-spl_autoload_register(function ($class): void {
-    $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
-    if (file_exists($file))
-        require $file;
-});
-
-// HTML-заголовок и стили
 echo "<!DOCTYPE html>
 <html lang='ru'>
 <head>
     <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>User Info</title>
+    <title>User Info (Lab 7)</title>
     <link rel=\"stylesheet\" href=\"css/style.css\">
-    
 </head>
 <body>";
 
+// Создание обычных пользователей
 $user1 = new User("Дима", "dimon355", "password1");
 $user2 = new User("Влад", "vladick345", "password2");
 $user3 = new User("Макс", "maks223", "password3");
@@ -30,12 +31,11 @@ echo $user1->showInfo();
 echo $user2->showInfo();
 echo $user3->showInfo();
 
-$user = new SuperUser("Admin", "mega_admin", "password4", "administrator");
-echo $user->showInfo();
+// Создание суперпользователя
+$userAdmin = new SuperUser("Admin", "mega_admin", "password4", "administrator");
+echo $userAdmin->showInfo();
 
-unset($user1);
-unset($user2);
-unset($user3);
-unset($user);
+// Принудительное удаление объектов для срабатывания деструкторов
+unset($user1, $user2, $user3, $userAdmin);
 
 echo "</body></html>";
